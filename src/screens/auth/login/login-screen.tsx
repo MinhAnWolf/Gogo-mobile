48bbbe85959b423c7c451f2df5a69ed8152f8eb9
Minh An <anltm@teamsolutions.vn>
2024-10-16 15:10:59 +0700

    dev on web

--- src/screens/auth/login/login-screen.tsx
+++ src/screens/auth/login/login-screen.tsx
@@ -1,13 +1,14 @@
-import React, { useState } from 'react';
-import { SubmitErrorHandler, useForm } from 'react-hook-form';
-import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { BaseStyles } from '../../../common/base-styles';
-import { Input } from '../../../component/component-common';
-import { NotiModal } from '../../../component/modal/notification-modal';
-import { InputType } from '../../../type/type';
-import { RequestAuthentication } from '../../../type/type-api';
-import { ScreenNavigationProp } from '../../../type/type-screen';
-import { LoginStyles } from './login-styles';
+import React, { useEffect, useState } from "react";
+import { SubmitErrorHandler, useForm } from "react-hook-form";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { BaseStyles } from "../../../common/base-styles";
+import { Input } from "../../../component/component-common";
+import { NotiModal } from "../../../component/modal/notification-modal";
+import { InputType } from "../../../type/type";
+import { RequestAuthentication } from "../../../type/type-api";
+import { ScreenNavigationProp } from "../../../type/type-screen";
+import { LoginStyles } from "./login-styles";
+import messaging from "@react-native-firebase/messaging";
 
 const LoginScreen: React.FC<ScreenNavigationProp> = ({ navigation }) => {
   const {
@@ -16,33 +17,34 @@ const LoginScreen: React.FC<ScreenNavigationProp> = ({ navigation }) => {
     handleSubmit,
     control,
     reset,
-    formState: { errors }
+    formState: { errors },
   } = useForm();
   // Declare state
   const [modalVisible, setModalVisible] = useState(false);
 
   // Declare input
   const email: InputType = {
-    name: 'email',
+    name: "email",
     styles: [BaseStyles.input, BaseStyles.mrBot15],
     required: true,
-    secureTextEntry: false
+    secureTextEntry: false,
   };
 
   const password: InputType = {
-    name: 'password',
+    name: "password",
     styles: [BaseStyles.input, BaseStyles.mrBot15],
     required: true,
-    secureTextEntry: true
+    secureTextEntry: true,
   };
 
   // handle event
   const onSubmit = (data: any) => {
     const reqData: RequestAuthentication = {
       email: data.email,
-      password: data.password
+      password: data.password,
     };
-    navigation.navigate('HomeScreen');
+    navigation.navigate("HomeScreen");
+    requestNotification();
     // login(reqData)
     //   .then((res) => {
     //     if (res) {
@@ -69,16 +71,37 @@ const LoginScreen: React.FC<ScreenNavigationProp> = ({ navigation }) => {
     setModalVisible(false);
   };
 
+  async function requestNotification() {
+    const requestUserPermission = async () => {
+      const authStatus = await messaging().requestPermission();
+      const enabled =
+        authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+        authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+      if (enabled) {
+        console.log("Authorization status:", authStatus);
+      }
+    };
+
+    const getToken = async () => {
+      const token = await messaging().getToken();
+      console.log("FCM Token:", token);
+    };
+
+    requestUserPermission();
+    getToken();
+  }
+
   return (
     <>
       <View style={[BaseStyles.container, BaseStyles.mrTop40]}>
         <View style={[LoginStyles.header]}>
           <Image
             source={{
-              uri: 'https://img.freepik.com/premium-photo/food-logo-illustration-illustration-white-background_961147-9216.jpg'
+              uri: "https://img.freepik.com/premium-photo/food-logo-illustration-illustration-white-background_961147-9216.jpg",
             }}
             style={[BaseStyles.image, BaseStyles.containerRelative]}
-            resizeMode='cover' // Adjust based on how you want to fit the image
+            resizeMode="cover" // Adjust based on how you want to fit the image
           />
         </View>
         <View style={[LoginStyles.content]}>
@@ -89,38 +112,63 @@ const LoginScreen: React.FC<ScreenNavigationProp> = ({ navigation }) => {
           <View style={[LoginStyles.formatScreenLogin]}>
             <Input inputType={password} control={control} />
           </View>
-          <View style={[LoginStyles.formatScreenLogin, BaseStyles.boderRadius10]}>
-            <TouchableOpacity style={styles.button} onPress={handleSubmit(onSubmit)}>
+          <View
+            style={[LoginStyles.formatScreenLogin, BaseStyles.boderRadius10]}
+          >
+            <TouchableOpacity
+              style={styles.button}
+              onPress={handleSubmit(onSubmit)}
+            >
               <Text style={styles.buttonText}>Login here</Text>
             </TouchableOpacity>
           </View>
-          <Text style={[BaseStyles.centerText, BaseStyles.mrTop20, BaseStyles.mrBot20]}>Forgot password</Text>
+          <Text
+            style={[
+              BaseStyles.centerText,
+              BaseStyles.mrTop20,
+              BaseStyles.mrBot20,
+            ]}
+          >
+            Forgot password
+          </Text>
           {/* BUTTON GOOGLE AND FACEBOOK */}
           <View style={[BaseStyles.rowCenter]}>
-            <TouchableOpacity style={styles.buttonImg} onPress={() => alert('Feature develop')}>
+            <TouchableOpacity
+              style={styles.buttonImg}
+              onPress={() => alert("Feature develop")}
+            >
               <Image
                 source={{
-                  uri: 'https://img.icons8.com/?size=100&id=17949&format=png&color=000000'
+                  uri: "https://img.icons8.com/?size=100&id=17949&format=png&color=000000",
                 }}
                 style={[BaseStyles.image, BaseStyles.containerRelative]}
-                resizeMode='contain' // Adjust based on how you want to fit the image
+                resizeMode="contain" // Adjust based on how you want to fit the image
               />
             </TouchableOpacity>
 
-            <TouchableOpacity style={styles.buttonImg} onPress={() => alert('Feature develop')}>
+            <TouchableOpacity
+              style={styles.buttonImg}
+              onPress={() => alert("Feature develop")}
+            >
               <Image
                 source={{
-                  uri: 'https://img.icons8.com/?size=100&id=118497&format=png&color=000000'
+                  uri: "https://img.icons8.com/?size=100&id=118497&format=png&color=000000",
                 }}
                 style={[BaseStyles.image, BaseStyles.containerRelative]}
-                resizeMode='contain' // Adjust based on how you want to fit the image
+                resizeMode="contain" // Adjust based on how you want to fit the image
               />
             </TouchableOpacity>
           </View>
 
-          <Text style={[BaseStyles.centerText, BaseStyles.mrTop20, BaseStyles.mrBot20]}>
-            Don't have an account?{' '}
-            <TouchableOpacity onPress={() => navigation.navigate('Register')}>
+          <Text
+            style={[
+              BaseStyles.centerText,
+              BaseStyles.mrTop20,
+              BaseStyles.mrBot20,
+            ]}
+          >
+            Don't have an account?{" "}
+            <TouchableOpacity onPress={() => navigation.navigate("Register")}>
               <Text>Sign Up</Text>
             </TouchableOpacity>
           </Text>
@@ -130,9 +178,9 @@ const LoginScreen: React.FC<ScreenNavigationProp> = ({ navigation }) => {
       <NotiModal
         visible={modalVisible}
         onClose={closeModal}
-        title='Sorry :('
-        message='Request failed with status code 401'
-        nameBtn='TRY AGAIN'
+        title="Sorry :("
+        message="Request failed with status code 401"
+        nameBtn="TRY AGAIN"
       />
     </>
   );
@@ -140,21 +188,21 @@ const LoginScreen: React.FC<ScreenNavigationProp> = ({ navigation }) => {
 
 const styles = StyleSheet.create({
   button: {
-    backgroundColor: 'rgb(255, 91, 45)', // Màu nền của nút
+    backgroundColor: "rgb(255, 91, 45)", // Màu nền của nút
     padding: 10, // Khoảng cách bên trong nút
-    borderRadius: 10 // Góc bo tròn của nút
+    borderRadius: 10, // Góc bo tròn của nút
   },
   buttonText: {
-    color: '#FFFFFF', // Màu văn bản
+    color: "#FFFFFF", // Màu văn bản
     fontSize: 16, // Kích thước văn bản
-    textAlign: 'center', // Căn giữa văn bản
-    fontWeight: 'bold'
+    textAlign: "center", // Căn giữa văn bản
+    fontWeight: "bold",
   },
   buttonImg: {
     width: 100,
     height: 50,
     borderRadius: 10, // Góc bo tròn của nút
-    margin: 10
-  }
+    margin: 10,
+  },
 });
 export default LoginScreen;
